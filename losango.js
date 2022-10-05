ecfb20be6a0c99e22aaafba8d1462f9ec956c11e
Núbia <112019351+nu-Bia@users.noreply.github.com>
2022-10-05 15:20:07 -0300

    Add files via upload
    10) Escreva um algoritmo para ler as dimensões de um losango (diagonal maior, diagonal menor), calcular e escrever a área do losango. Dica a=D.d/2

---
+++ losango.js
@@ -0,0 +1,6 @@
+function multiplique(){
+    var numero1=prompt(("digite o comprimento da diagonal maior:"));
+    var numero2=(prompt("digite o comprimento da diagonal menor"));
+    var multiplique=numero1*numero2/2;
+    alert(" O valor da multiplicação é :"+multiplique);
+    }
